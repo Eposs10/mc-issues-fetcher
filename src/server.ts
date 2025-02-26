4b30fca9512dc7ab422170ea8eb557b9650a1eb1
Eposs10 <lunalaluna592@gmail.com>
2025-02-26 16:30:26 +0100

    Add fetch

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import Express, { Request, Response } from "express";
 import Path from "path";
 import logger from "./logging";
 import fs from "fs";
-import { validateIssueId } from "./util";
+import { fetchRenderedHTML, validateIssueId } from "./util";
 
 const app = Express();
 const port = 4001;
@@ -14,21 +14,28 @@ app.use(logger);
 app.use(Express.json());
 
 // Routes
-app.get("/:issue", (req: Request, res: Response) => {
+app.get("/:issue", async (req: Request, res: Response) => {
     const issueId = req.params.issue;
     if (!validateIssueId(issueId)) {
         res.sendStatus(400);
         return;
     }
 
+    const issueUrl = mojiraUrl + issueId;
+    const fetch = await fetchRenderedHTML(issueUrl);
+
+    const titleStart = fetch.indexOf("<h4 class=\"MuiTypography-root MuiTypography-h4 css-1klm1w7\">"); // length = 60
+    const titleEnd = fetch.indexOf("</h4>", titleStart);
+    const title = fetch.substring(titleStart + 60, titleEnd);
+    console.log(title);
+
     let html = fs.readFileSync(pathPublic + '/template.html', 'utf-8');
 
     html = html.replaceAll('$title', issueId);
-    html = html.replaceAll('$issue-title', `Title: ${issueId}`);
-    html = html.replaceAll('$issue-description', `Description: ${issueId}`);
-    html = html.replaceAll('$issue-link', mojiraUrl + issueId);
+    html = html.replaceAll('$issue-title', `[${issueId}]`);
+    html = html.replaceAll('$issue-description', title);
+    html = html.replaceAll('$issue-link', issueUrl);
     html = html.replaceAll('$issue-id', issueId);
-
 
     res.status(200).send(html);
 });
--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,36 @@
+import puppeteer from "puppeteer";
+
 export function validateIssueId(issueId: string): boolean {
     if (!issueId) return false;
     if (!issueId.startsWith("MC-")) return false;
-    if (issueId.length < 4) return false; 
-    
+    if (issueId.length < 4) return false;
+
     const idPart = issueId.split("-")[1];
     return isNumericRegex(idPart);
-    
-    
+
+
 }
 
 function isNumericRegex(str: string): boolean {
     return /^\d+$/.test(str);
+}
+
+export async function fetchHTML(url: string): Promise<string> {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.text();
+}
+
+export async function fetchRenderedHTML(url: string): Promise<string> {
+    const browser = await puppeteer.launch({headless: true}); // Runs in headless mode
+    const page = await browser.newPage();
+
+    await page.goto(url, {waitUntil: "networkidle2"}); // Waits for the page to fully load
+
+    const html = await page.content(); // Gets the fully rendered HTML
+    await browser.close();
+
+    return html;
 }
