f23b86cebdc8d4edc0223a4295ebcdb3c1939104
Eposs10 <lunalaluna592@gmail.com>
2025-02-26 16:06:49 +0100

    Add string replacement for template

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import Express, { Request, Response } from "express";
 import Path from "path";
 import logger from "./logging";
 import fs from "fs";
-import dayjs from "dayjs";
 import { validateIssueId } from "./util";
 
 const app = Express();
@@ -18,11 +17,20 @@ app.use(Express.json());
 app.get("/:issue", (req: Request, res: Response) => {
     const issueId = req.params.issue;
     if (!validateIssueId(issueId)) {
-        res.sendStatus(404);
+        res.sendStatus(400);
         return;
     }
-    
-    res.status(200).sendFile(pathPublic + '/template.html');
+
+    let html = fs.readFileSync(pathPublic + '/template.html', 'utf-8');
+
+    html = html.replaceAll('$title', issueId);
+    html = html.replaceAll('$issue-title', `Title: ${issueId}`);
+    html = html.replaceAll('$issue-description', `Description: ${issueId}`);
+    html = html.replaceAll('$issue-link', mojiraUrl + issueId);
+    html = html.replaceAll('$issue-id', issueId);
+
+
+    res.status(200).send(html);
 });
 
 // Start the server
